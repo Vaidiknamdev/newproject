574c583b060b30370f1648a152874ba4fa24eda9
Vaidiknamdev <vaidiknamdev4530@gmail.com>
2024-07-11 12:06:15 +0530

    changes done one more

--- src/java/SQLQuerys.java
+++ src/java/SQLQuerys.java
@@ -2,3 +2,4 @@
 CREATE DATABASE ProjectData
 CREATE TABLE Student(userid varchar(10) primary,password varchar(10),name varchar(10),address varchar(10),email varchar(10),mobile varchar(10));
 */
+//hello this change made
